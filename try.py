35a94a6a821894cc8cfb4ae4ef00d101ab0b7a92
ChentingZ0 <chentingzzzz@gmail.com>
2023-02-25 20:50:25 +0100

    add signal quantization

--- try.py
+++ try.py
@@ -13,3 +13,37 @@ print(c.shape,'c')  # Output: (2, 1, 2)
 
 
 print(np.log([1, np.e, np.e**2, 0]),'ln')
+
+r3 = np.arange(0, 11.51, 0.01)
+r = np.arange(0,5,1)
+print(r3)
+
+
+# idx = [sq_enc(x, r, x_max_list[i],m) for i in x_max_list]
+# out1 = [sq_dec(idx[i])]
+#
+
+x = [1,5,7,9]
+def times(x):
+    y = 2*x
+    return y
+list = []
+
+
+list1 = [times(x[i]) for i in range(0,len(x))]
+print(list1)
+
+list2 = [times(i) for i in x]
+print(list2)
+
+print(np.array([3,4,5,6,7])-np.array([1,2,3,4,5]))
+
+
+outq_array = np.array([[1,2,3],[3,4,5]])
+print(outq_array.shape)
+print(outq_array[1,:])
+print(outq_array[1,:].shape)
+
+A = [4, 6, 8, 10]
+B = [2, 3, 4, 5]
+print(A/B)
